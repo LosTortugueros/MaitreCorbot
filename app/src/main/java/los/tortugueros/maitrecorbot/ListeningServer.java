9d04b2e34f02573771a2b5adf4ece39e52f01dba
Mickael Goubin <micky0304@gmail.com>
2014-12-04 21:07:26 +0100

    Init repo

---
+++ app/src/main/java/los/tortugueros/maitrecorbot/ListeningServer.java
@@ -0,0 +1,12 @@
+package los.tortugueros.maitrecorbot;
+
+import android.app.Activity;
+
+/**
+ * Created by MagicMicky on 04/12/2014.
+ */
+public class ListeningServer {
+    public ListeningServer(Activity act) {
+
+    }
+}
---
+++ app/src/main/java/los/tortugueros/maitrecorbot/OnMessageReceived.java
@@ -0,0 +1,9 @@
+package los.tortugueros.maitrecorbot;
+
+/**
+ * Created by MagicMicky on 04/12/2014.
+ */
+public interface OnMessageReceived {
+
+    public void onMessage(String message);
+}
